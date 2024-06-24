471f97835cfc570f8fe96ad468145aa6c7095ff2
javierpa95 <javierpenatearrieta@gmail.com>
2024-06-24 20:07:16 +0200

    Add MAGE, M-Value y Lability_index

--- cgmpy/cgmpy.py
+++ cgmpy/cgmpy.py
@@ -1,17 +1,17 @@
 import datetime as datetime
 import numpy as np
 import matplotlib.pyplot as plt
-
 import pandas as pd
 
 class GlucoseData:
-    def __init__(self, file_path, date_col, glucose_col, delimiter=',', header=0):
+    def __init__(self, file_path, date_col, glucose_col, date_format='%d/%m/%Y %H:%M', delimiter=',', header=0):
         """
         Inicializa la clase con los datos de glucemia a partir de un archivo CSV.
         
         :param file_path: Ruta al archivo CSV.
         :param date_col: Nombre de la columna que contiene las fechas.
         :param glucose_col: Nombre de la columna que contiene los valores de glucosa.
+        :param date_format: Formato de fecha esperado en la columna de fechas (por defecto '%d/%m/%Y %H:%M').
         :param delimiter: Delimitador usado en el archivo CSV (por defecto ',').
         :param header: Índice de la fila que contiene los nombres de las columnas (por defecto 0).
         """
@@ -30,7 +30,6 @@ class GlucoseData:
             # Cargar el archivo CSV con el delimitador especificado
             self.data = pd.read_csv(file_path, delimiter=delimiter, header=header)
 
-
         # Verificar que las columnas especificadas existen
         if date_col not in self.data.columns or glucose_col not in self.data.columns:
             # Cargar el archivo para obtener las columnas
@@ -39,8 +38,8 @@ class GlucoseData:
 
         # Eliminar filas donde date_col sea nulo o vacío
         self.data = self.data.dropna(subset=[date_col])
-        # Convertir la columna de fechas a datetime
-        self.data[date_col] = pd.to_datetime(self.data[date_col], errors='coerce')
+        # Convertir la columna de fechas a datetime con formato especificado
+        self.data[date_col] = pd.to_datetime(self.data[date_col], format=date_format, errors='coerce')
         # Filtrar las filas que contienen datos de glucosa válidos
         self.data = self.data.dropna(subset=[date_col, glucose_col])
         # Asegurarnos de que la columna de glucosa es numérica
@@ -53,6 +52,10 @@ class GlucoseData:
         self.glucose_col = 'glucose'
         # Seleccionar solo las columnas 'time' y 'glucose'
         self.data = self.data[['time', 'glucose']]
+
+        print("Hay en total", len(self.data), "datos.")
+        print("las fechas van entre",self.data['time'].min(),"y ", self.data['time'].max())
+
 
     def mean(self):
         """
@@ -142,7 +145,7 @@ class GlucoseData:
     
     # MEDIDAS DE VARIABILIDAD 
     
-    def conga(self, min=5, hours=24):
+    def CONGA(self, min=5, hours=24):
         """
         Calcula y devuelve el valor de CONGA para datos medidos cada 5 minutos.
         :param min: Distancia en minutos entre las mediciones. Por defecto 5 minutos
@@ -176,7 +179,7 @@ class GlucoseData:
         print(f"Para el calculo de CONGA{hours}, hay {intervals} valores que no tienen previo y se han eliminado {len(self.data)-k_star}")
         return conga_n
 
-    def modd(self, min = 5):
+    def MODD(self, min = 5):
         """
         Calcula y devuelve el valor de MODD para datos medidos cada 5 minutos (por defecto).
         DOI: 10.1007/BF01218495
@@ -203,7 +206,6 @@ class GlucoseData:
         print(f"Para el calculo de modd, hay {intervals} valores que no tienen previo y se han eliminado {len(self.data)-k_star}")
         
         return modd
-
 
     def j_index(self):
 
@@ -247,7 +249,147 @@ class GlucoseData:
         # Calcular y devolver HBGI
         return self.data["rh_bg"].mean()
 
+    def MAGE(self):
+        """
+        Calcula el MAGE (Mean Amplitude of Glycemic Excursions).
+        
+        :return: El valor de MAGE.
+        """
+        # Paso 1: Calcular la desviación estándar (SD) de los valores de glucosa
+        sd = self.sd()
+        
+        # Paso 2: Determinar todos los valores máximos (picos) y mínimos (valles) locales
+        # Utilizando el método shift para comparar cada valor con el anterior y el siguiente
+        peaks_and_nadirs = self.data[
+            (self.data['glucose'].shift(1) < self.data['glucose']) & (self.data['glucose'] > self.data['glucose'].shift(-1)) | 
+            (self.data['glucose'].shift(1) > self.data['glucose']) & (self.data['glucose'] < self.data['glucose'].shift(-1))
+        ]
+        
+        # Restablecer el índice para trabajar con índices secuenciales
+        peaks_and_nadirs.reset_index(drop=True, inplace=True)
+        
+        # Lista para almacenar las excursiones glucémicas válidas
+        excursions = []
+        
+       # Paso 3: Verificar si el primer valor es un pico o un valle
+        starts_with_peak = peaks_and_nadirs['glucose'][0] > peaks_and_nadirs['glucose'][1]
+
+        # Evaluar los pares de pico y valle para determinar si cumplen con el criterio de 1 SD
+        if starts_with_peak:
+            for i in range(0, len(peaks_and_nadirs) - 1, 2):
+                peak = peaks_and_nadirs['glucose'][i]
+                nadir = peaks_and_nadirs['glucose'][i + 1]
+                # Verificar si la diferencia entre pico y valle excede 1 SD
+                if abs(peak - nadir) > sd:
+                    excursions.append(abs(peak - nadir))
+        else:
+            for i in range(0, len(peaks_and_nadirs) - 1, 2):
+                nadir = peaks_and_nadirs['glucose'][i]
+                peak = peaks_and_nadirs['glucose'][i + 1]
+                # Verificar si la diferencia entre valle y pico excede 1 SD
+                if abs(peak - nadir) > sd:
+                    excursions.append(abs(peak - nadir))
+
+        
+        # Si no se encontraron excursiones válidas, retornar 0
+        if len(excursions) == 0:
+            return 0
+        print(excursions)
+        # Paso 4: Calcular el MAGE promediando las excursiones glucémicas válidas
+        print("En mage 2 hay:", len(excursions),"y en total",len(peaks_and_nadirs))
+        return sum(excursions) / len(excursions)
     
+    def M_Value(self, target_glucose=80):
+        """
+        Calcula el M-Value para evaluar la variabilidad de la glucosa en sangre.
+
+        :param target_glucose: Valor objetivo de glucosa (por defecto 80 mg/dL).
+        :return: El M-Value.
+
+        DOI: 10.2337/db12-1396 - Explica que si tienes mas de 25 datos puedes obviar la corrección. Además el target_glucose en el artículo original lo ponínan en 120, ellos ponen 90!!!
+        """
+        def calculate_M(PG):
+            return abs(10 * np.log10(PG / target_glucose)) ** 3
+
+        self.data['M'] = self.data['glucose'].apply(calculate_M)
+        M_sum = self.data['M'].sum()
+        N = len(self.data)
+
+        
+        M_value = (M_sum / N) 
+        
+        return M_value
+
+    def Lability_index(self, interval=1, period='week'):
+        """
+        Calcula el índice de labilidad (LI) basado en las lecturas de glucosa y sus tiempos correspondientes.
+
+        :param interval: El intervalo de tiempo en horas para el cálculo.
+        :param period: El periodo para la media del LI ('day', 'week', 'month').
+        :return: El índice de labilidad (LI) medio para el periodo especificado.
+        """
+        # Trabajar con una copia del DataFrame original
+        data_copy = self.data.copy()
+        data_copy.set_index('time', inplace=True)
+        
+        # Imprimir el rango de fechas antes del resampleo
+        print("Rango de fechas antes del resampleo:", data_copy.index.min(), "a", data_copy.index.max())
+        
+        # Resamplear los datos a intervalos especificados
+        resampled_data = data_copy.resample(f'{interval}H').asfreq().dropna().reset_index()
+        
+        # Imprimir el rango de fechas después del resampleo
+        print("Rango de fechas después del resampleo:", resampled_data['time'].min(), "a", resampled_data['time'].max())
+        
+        print("Número de filas en los datos resampleados:", len(resampled_data))
+        print(resampled_data.head())
+        print(resampled_data.tail())
+        
+        # Asignar periodos según el tipo especificado (día, semana, mes)
+        if period == 'day':
+            resampled_data['period'] = resampled_data['time'].dt.date
+        elif period == 'week':
+            # Calcular manualmente la semana basada en el primer dato
+            resampled_data['period'] = (resampled_data['time'] - resampled_data['time'].min()).dt.days // 7
+        elif period == 'month':
+            resampled_data['period'] = resampled_data['time'].dt.to_period('M').apply(lambda r: r.start_time)
+        else:
+            raise ValueError("Periodo no válido. Usa 'day', 'week' o 'month'.")
+        
+        print(resampled_data.head())
+        
+        li_values = []
+        
+        # Calcular el índice de labilidad para cada grupo de periodos
+        for period, group in resampled_data.groupby('period'):
+            print(f"Período: {period}, Número de datos: {len(group)}")
+            glucose_readings = group['glucose'].values
+            times = group['time'].values.astype('datetime64[h]').astype(int)  # Convertir tiempos a horas y luego a enteros
+            
+            if len(glucose_readings) < 2:
+                continue
+            
+            n = len(glucose_readings)
+            li_sum = 0
+            
+            # Calcular el LI para el grupo actual
+            for i in range(n - 1):
+                gluc_diff = glucose_readings[i] - glucose_readings[i + 1]
+                time_diff = times[i + 1] - times[i]
+                
+                if 1 <= time_diff <= interval:
+                    li_sum += (gluc_diff ** 2) / time_diff
+            
+            li_value = li_sum / (n - 1)
+            print(f"LI para el grupo {period}: {li_value}")
+            li_values.append(li_value)
+        
+        if not li_values:
+            return 0
+        
+        return np.mean(li_values)
+
+
 
     # GRAFICOS
     def generate_agp(self):
